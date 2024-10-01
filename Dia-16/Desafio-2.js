e5b32052499057fd02d6da01ab6f15568235ce58
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-01 09:58:29 -0300

    Desafio 2 finalizado

--- Dia-16/Desafio-2.js
+++ Dia-16/Desafio-2.js
@@ -1,5 +1,13 @@
-/*
-### Exercícios Nível 2
+//### Exercícios Nível 2
 
-1. Converta o objeto estudantes para JSON, incluindo apenas as propriedades firstName, lastName e skills 
-*/
+//1. Converta o objeto estudantes para JSON, incluindo apenas as propriedades firstName, lastName e skills 
+const student = {
+    firstName:'Asabeneh',
+    lastName:'Yetayehe',
+    age:250,
+    isMarried:true,
+    skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+  
+  }
+    const Student = JSON.stringify(student, ['firstName', 'lastName', 'skills']);
+    console.log(Student);
