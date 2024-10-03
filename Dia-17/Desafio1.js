fcf3b5a9ed0d05127c2ccf7e1ea56df736d928ab
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-03 10:11:24 -0300

    desaio 1 feito

--- Dia-17/Desafio1.js
+++ Dia-17/Desafio1.js
@@ -1,3 +1,29 @@
 /* ### Exercícios: Nível 1
 1. Armazene seu primeiro nome, sobrenome, idade, país e cidade no localStorage do seu navegador. 
  */
+
+//feito pelo consolo da web
+localStorage.setItem('Nome','Gabriela donato');
+undefined
+localStorage.setItem('Sobrenome','Souza');
+undefined
+localStorage.setItem('Idade', '25');
+undefined
+localStorage.setItem('Pais', 'Brasil');
+undefined
+localStorage.setItem('Cidade', 'Guarujá');
+undefined
+
+localStorage.getItem('Nome')
+'Gabriela donato'
+localStorage.getItem('Idade')
+'25'
+localStorage.getItem('Sobrenome')
+'Souza'
+localStorage.getItem('Pais')
+'Brasil'
+localStorage.getItem('Cidade')
+'Guarujá'
+
+
+
--- Dia-17/readme.md
+++ Dia-17/readme.md
@@ -4,6 +4,8 @@
 
 1. Armazene seu primeiro nome, sobrenome, idade, país e cidade no localStorage do seu navegador.
 
+![alt text](image.png)
+
 ### Exercícios: Nível 2
 
 1. Crie um objeto estudante. O objeto estudante terá as chaves e valores de primeiro nome, sobrenome, idade, habilidades, país e matrícula. Armazene o objeto estudante no localStorage do seu navegador.
