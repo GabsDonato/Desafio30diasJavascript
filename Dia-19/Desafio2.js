3dc18949a82998a9e8ad837632b9ef2109f449b4
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-08 19:55:46 -0300

    Desafio 2 finalizado

--- Dia-19/Desafio2.js
+++ Dia-19/Desafio2.js
@@ -3,3 +3,22 @@
 
 1. Crie um closure que tenha três funções internas. 
 */
+
+function Alfabeto(){
+    let letras = 'a';
+    function Vogais(){
+      letras = String.fromCharCode(letras.charCodeAt(0) + 1);
+      return letras;
+    }
+    function Consoantes(){
+      return letras
+    }
+    return {
+      Vogais,
+      Consoantes,
+    };
+  }
+  const alfabeto = Alfabeto();
+  console.log(alfabeto.Vogais());
+  console.log(alfabeto.Vogais());
+  console.log(alfabeto.Vogais());
