f91efd6f0cc2733e4f13ea1b8a1475542fc91898
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-08 19:48:48 -0300

    Desafio 1 finalizado

--- Dia-19/Desafio1.js
+++ Dia-19/Desafio1.js
@@ -4,3 +4,21 @@
 1. Crie um closure que tenha uma função interna.
  
 */
+
+function Contar() {
+    let count = 0;
+    function Aumentar(){
+      count++
+      return count
+    }
+    return Aumentar;
+  
+  }
+  
+  const Soma = Contar();
+  console.log(Soma())
+  console.log(Soma())
+  console.log(Soma())
+  console.log(Soma())
+  console.log(Soma())
+  console.log(Soma())
