cc1db64b9cf7c53e13c00d22a49224db0ff4b761
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-10 19:10:01 -0300

    Desafio 2 feito

--- Dia-21/Desafio2.js
+++ Dia-21/Desafio2.js
@@ -6,3 +6,40 @@
 3. Defina o conteúdo de texto, id e class para cada parágrafo.
 
  */
+
+const paragrafoUm = document.querySelectorAll('#Primeiro-p p'); 
+
+paragrafoUm.forEach((paragraf, i) => {
+  paragraf.style.fontSize = '24px';
+  paragraf.style.color = (i % 2 === 0) ? 'green' : 'red';
+});
+
+const paragrafoDois = document.querySelectorAll('#Segundo-p p'); 
+
+paragrafoDois.forEach((paragraf, i) => {
+  paragraf.style.fontSize = '24px';
+  paragraf.style.color = (i % 2 === 0) ? 'blue' : 'red';
+});
+
+const paragrafosTres = document.querySelectorAll('#Terceiro-p p');
+paragrafosTres.forEach((paragraf, i) => {
+  paragraf.style.fontSize = '24px';
+  paragraf.style.color = (i % 2 === 0) ? 'purple' : 'orange'; 
+});
+
+const paragrafosQuatro = document.querySelectorAll('#Quarto-p p');
+paragrafosQuatro.forEach((paragraf, i) => {
+  paragraf.style.fontSize = '24px';
+  paragraf.style.color = (i % 2 === 0) ? 'brown' : 'yellow'; 
+});
+
+const paragrafos = document.querySelectorAll('p');
+
+paragrafos.forEach((paragraf, i) => {
+  paragraf.style.color = (i === 0 || i === 2) ? 'green' : 'red';
+
+
+    paragraf.textContent = `Este é o parágrafo ${i + 1}`;
+    paragraf.id = `paragrafo-${i + 1}`; 
+    paragraf.className = `classe-paragrafo-${i + 1}`;
+    });
